e02f1ad7b000d65103762fc3b62681728018f7ea
pi3rrot <4dakto@gmail.com>
2018-06-02 23:23:10 +0900

    Persisting the Redux Store

---
+++ redux/modules/user.js
@@ -0,0 +1,29 @@
+// imports
+
+// actions
+
+// action creator
+
+// api actions
+
+// initial state
+const initialState = {
+  isLoggedIn: false,
+}
+
+// reducer
+
+// reducer function
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+// exports
+
+
+// defuat reducer export
+
+export default reducer;
